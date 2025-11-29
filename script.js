f51b0d6ec959d72113e992868ba27f22903e1d90
SumonReza8525 <sumon.esd.bsmrstu@gmail.com>
2025-11-29 15:24:11 +0600

    recaped all the functionalities

--- script.js
+++ script.js
@@ -1,25 +1,26 @@
-const container = document.getElementById("container");
-const cards = document.querySelectorAll("#container .card");
-
+const cards = document.querySelectorAll(".card");
+const cardButtons = document.querySelectorAll(".cardBtn");
 const data = [];
 
-for (let n = 0; n < cards.length; n++) {
-  cards[n].addEventListener("click", () => {
-    for (let i = 0; i < cards.length; i++) {
-      cards[i].classList.remove("bg-red-300");
+for (let n = 0; n < cardButtons.length; n++) {
+  cardButtons[n].addEventListener("click", () => {
+    for (let i = 0; i < cardButtons.length; i++) {
+      cards[i].classList.remove("bg-cyan-400");
     }
-    cards[n].classList.add("bg-red-300");
-    const name = cards[n].querySelector("h1").innerText;
-    const image = cards[n].querySelector("img").getAttribute("src");
-    let price = cards[n].querySelector(".price").innerText;
-
-    const information = { name: name, image: image, price: price };
-    data.unshift(information);
+    cards[n].classList.add("bg-cyan-400");
 
     const historyDiv = document.getElementById("historyDiv");
+    const newDivCreate = document.createElement("div");
 
-    const historyNew = document.createElement("div");
-    historyNew.innerHTML = `<div
+    // card details
+    let imageSrc = cards[n].querySelector("img").getAttribute("src");
+    let name = cards[n].querySelector("h1").innerText;
+    let price = cards[n].querySelector(".price").innerText;
+
+    const cardDetails = { name: name, image: imageSrc, price: price };
+    data.unshift(cardDetails);
+
+    newDivCreate.innerHTML = `<div
               class="bg-cyan-200 p-4 rounded-lg flex justify-between items-center"
             >
               <img class="w-[100px]" src='${data[0].image}' alt="" />
@@ -28,22 +29,21 @@ for (let n = 0; n < cards.length; n++) {
                 <p class="text-gray-400 text-[20px]">${data[0].price}</p>
               </div>
             </div>`;
-    historyDiv.appendChild(historyNew);
+    historyDiv.appendChild(newDivCreate);
 
-    let totalPrice = document.getElementById("totalPrice");
-    let discountPrice = document.getElementById("discount");
-    let finalPrice = document.getElementById("afterDiscount");
-    // console.log(finalPrice.innerText);
+    let cardPrice = data[0].price;
+    let cardPriceNumber = parseFloat(cardPrice);
+    let totalPriceSpan = document.getElementById("totalPrice").innerText;
+    let totalPriceSpanNumber = parseFloat(totalPriceSpan);
+    let discountSpan = document.getElementById("discount");
+    let priceAfterDiscount = document.getElementById("afterDiscount");
 
-    let priceInNumber = parseFloat(totalPrice.innerText);
-    let eachCardPrice = data[0].price;
-    let eachPriceInNumber = parseFloat(eachCardPrice);
-    let total = priceInNumber + eachPriceInNumber;
+    let total = (cardPriceNumber + totalPriceSpanNumber).toFixed(2);
     let discount = (total * 0.1).toFixed(2);
-    discountPrice.innerText = `${discount}TK`;
-    totalPrice.innerText = `${total}TK`;
+    let netAmount = total - discount;
 
-    let netPrice = (total - discount).toFixed(2);
-    finalPrice.innerText = `${netPrice}TK`;
+    document.getElementById("totalPrice").innerText = `${total}TK`;
+    discountSpan.innerText = `${discount}TK`;
+    priceAfterDiscount.innerText = `${netAmount}TK`;
   });
 }
